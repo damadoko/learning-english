c2bb147450a4ca6569df943bf90a3f18012ce9db
Le Minh Triet <lmtrietct@gmail.com>
2025-06-14 09:21:18 +0700

    Add scroll to bottom

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Box } from "@mui/material";
 
 import type { Message } from "../types";
@@ -8,13 +9,20 @@ export type ChatBoxProps = {
 };
 
 export const ChatBox: React.FC<ChatBoxProps> = ({ messages }) => {
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <Box
       sx={{ flexGrow: 1, overflowY: "auto", px: 3, py: 2, bgcolor: "#f5f5f5" }}
     >
       {messages.map((msg) => (
-        <MessageItem message={msg} />
+        <MessageItem key={msg.id} message={msg} />
       ))}
+      <div ref={bottomRef} />
     </Box>
   );
 };
